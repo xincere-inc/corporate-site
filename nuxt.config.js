4985580c0bc4b8f71683f8f658477068fe2cb844
shengbo.xu <shengbo.xu@xincere.jp>
2021-01-13 02:16:37 +0900

    remove main.scss

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,7 +40,7 @@ export default {
   env: envSet,
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: ["~assets/stylesheets/main.scss"],
+  css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
