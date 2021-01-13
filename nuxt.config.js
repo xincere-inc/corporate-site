3145660cb8cc12d9e45bfadca929320b3e84a97f
shengbo.xu <shengbo.xu@xincere.jp>
2021-01-13 12:08:24 +0900

    update og image

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,18 @@ export default {
     title: "株式会社シンシア",
     titleTemplate: "ライフステージの変化をサポートします | %s",
     meta: [
+      { hid: "og:type", property: "og:type", content: "website" },
+      {
+        hid: "og:description",
+        property: "og:description",
+        content:
+          "一般的にライフステージが変化するタイミング（入学・就職・結婚・出産）で、人は住まいを変えます。その重要な出来事で発生する  不動産の売買・賃貸  に伴う手続きなどを“”、我々はその時代における最善の方法を用いて、より簡単に・効率化していきます。"
+      },
+      {
+        hid: "og:image",
+        property: "og:image",
+        content: "https://corp.xincere.jp/og_image.png"
+      },
       {
         charset: "utf-8"
       },
