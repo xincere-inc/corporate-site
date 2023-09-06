bbeaffe3b9b62251febb2d26b890436ae4170be0
shengbo.xu <shengbo.xu@xincere.jp>
2023-09-06 20:15:00 +0900

    add base layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    screens: {
+      md: "768px",
+    },
+    fontSize: {
+      h2: "2.5rem",
+      eh2: "2rem",
+      h3: "1.5rem",
+    },
+    colors: {
+      white: "#FFFFFF",
+      gray: "#F0F0F0",
+      primary: "#518C3C",
+      black: "#000000",
+    },
+    fontFamily: {},
+    extend: {
+      width: {
+        'desktop': '1160px',
+      }
+    },
+  },
+  plugins: [],
+};
