edb392cb58cffdf393e9c1f7130ea3ec380fadfa
shengbo.xu <shengbo.xu@xincere.jp>
2023-10-05 10:23:13 +0900

    add digital partner

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,12 +9,15 @@ module.exports = {
       h2: "2.5rem",
       eh2: "2rem",
       h3: "1.5rem",
+      "5xl": ['3rem', 1],
+      "xl": ['1.25rem', '1.75rem'],
     },
     colors: {
       white: "#FFFFFF",
       gray: "#F0F0F0",
       primary: "#518C3C",
       black: "#000000",
+      primary10: "#F1F6F0"
     },
     fontFamily: {},
     extend: {
