3487fe609b7b0ddd1b2a76a30c78643bb98cf680
keigo-narita <143776995+keigo-narita@users.noreply.github.com>
2023-10-18 13:35:58 +0900

    Update blogs (#9)
    * update blogs.astro and blogs.css
    
    * Add Aside and apply Tailwind in blogs
    
    * 02_update blogs.astro and blogs.css
    
    * fix text-size, text-color, text-truncate
    
    * update office, recruit, company profile (#10)
    
    * update office, recruit, company profile
    
    * add 'About' and 'Value' sections
    
    * fix: H2の修正・採用情報セクションのコンポーネント化
    
    * fix: H2コンポーネントと会社概要セクションの修正
    
    * Update contact form (#11)
    
    * update office, recruit, company profile
    
    * add 'About' and 'Value' sections
    
    * update contact form
    
    * fix: 不要なinportやid、placeholderなどの削除
    
    ---------
    
    Co-authored-by: shengbo.xu <1964055+yamadagenki@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: shengbo.xu <1964055+yamadagenki@users.noreply.github.com>

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,17 +9,32 @@ module.exports = {
       h2: "2.5rem",
       eh2: "2rem",
       h3: "1.5rem",
+      xl: "1.25rem",
+      base: "1rem",
+      sm: "0.875rem",
+      xs: "0.75rem",
     },
     colors: {
       white: "#FFFFFF",
       gray: "#F0F0F0",
       primary: "#518C3C",
       black: "#000000",
+      body: "#333333",
+      Description: "#949495",
+      alert: "#FF0000",
+      "border/light": "#D6D6D6",
+      "gray/90": "#1E2D39",
     },
-    fontFamily: {},
+    fontFamily: {
+      gothic: ["YuGothic"],
+      sans: ["NotoSans"],
+    },
     extend: {
       width: {
         desktop: "1160px",
+      },
+      boxShadow: {
+        "custom-lg": "0px 2px 8px 0px rgba(0, 0, 0, 0.25)",
       },
     },
     variants: {
