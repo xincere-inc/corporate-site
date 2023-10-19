41177d50937b3572c91f23c90bbfafb5399cda57
shengbo.xu <shengbo.xu@xincere.jp>
2023-10-19 15:06:24 +0900

    add alpinejs/collapse

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import alpinejs from "@astrojs/alpinejs";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()]
+  integrations: [tailwind()]
 });
