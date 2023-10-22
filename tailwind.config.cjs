2fafc90a2bf172644cd0be0282504ff0f72bff07
shengbo.xu <shengbo.xu@xincere.jp>
2023-10-22 21:49:55 +0900

    ad just recruiting page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -42,7 +42,6 @@ module.exports = {
   },
   plugins: [
     require("@tailwindcss/forms"),
-    require("@tailwindcss/typography"),
-    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/typography")
   ],
 };
